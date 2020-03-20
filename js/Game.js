8d03c18108b404682925e69abf8571b86ea50aaf
JHG777000 <jhg777000@gmail.com>
2020-03-20 10:26:25 -0400

    add addPhraseToDisplay

--- js/Game.js
+++ js/Game.js
@@ -3,15 +3,25 @@
  * Game.js */
 
 class Game {
-    constructor() {
-        this.missed = 0;
-        this.phrases = [new Phrase('Hello World!')]
-        this.activePhrase = null;
+  constructor() {
+    this.missed = 0;
+    this.phrases = [new Phrase('Hello World!')];
+    this.activePhrase = null;
+  }
+  startGame() {
+    const overlay = document.getElementById('overlay');
+    overlay.style.display = 'none';
+    //overlay.style.display = '';
+
+    this.activePhrase = this.getRandomPhrase();
+    this.activePhrase.addPhraseToDisplay();
+  }
+  getRandomPhrase() {
+    let phrase = this.phrases[Math.floor(Math.random() * this.phrases.length)];
+    //loop until different from last color
+    while (phrase === this.activePhrase) {
+      phrase = this.phrases[Math.floor(Math.random() * this.phrases.length)];
     }
-    startGame() {
-        const overlay = document.getElementById('overlay');
-        overlay.style.display = 'none';
-        //overlay.style.display = '';
-        this.phrases[0].addPhraseToDisplay();
-    }
+    return phrase;
+  }
 }
--- js/Phrase.js
+++ js/Phrase.js
@@ -7,7 +7,28 @@ class Phrase {
       this.phrase = phrase.toLowerCase();
     }
     addPhraseToDisplay() {
-      alert(this.phrase);
+      let main_div = document.getElementsByClassName('main-container')[0];
+      let div = document.createElement('div');
+      let ul = document.createElement('ul');
+      let li = undefined;
+      
+      div.id = 'phrase';
+      div.class = 'section';
+      div.appendChild(ul);
+
+      for (let i = 0; i < this.phrase.length; i++) {
+        if (this.phrase.charAt(i) === ' ') {
+          li = document.createElement('li');
+          li.className = 'space';
+          li.innerText = ' ';
+        } else {
+          li = document.createElement('li');
+          li.className = 'hide letter ' + this.phrase.charAt(i);
+          li.innerText = this.phrase.charAt(i);
+        }
+        ul.appendChild(li);
+      }
+      main_div.prepend(div);
     }
   }
   
